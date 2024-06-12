64fd6d4b55ccdeaca5c819ba158d25f67f7a7bf2
GabrielHenriqueDev <discente.ifpe.edu.br>
2024-06-12 08:49:12 -0300

    Começando o projeto

---
+++ src/main/java/com/AfroPay/AfroPayApplication.java
@@ -0,0 +1,13 @@
+package com.AfroPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfroPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfroPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AfroPay/AfroPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AfroPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfroPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
